c2eee273939bcb0381e10d777a63277e505b1500
Fimics <lpjhblpj@163.com>
2019-03-04 00:39:49 +0800

    IntStream.range() closeRange ,,map..reduce

--- src/main/java/com/mic/java8/stream/StreamCreatedDemo.java
+++ src/main/java/com/mic/java8/stream/StreamCreatedDemo.java
@@ -2,19 +2,46 @@ package com.mic.java8.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+@SuppressWarnings("unused")
 public class StreamCreatedDemo {
     public static void main(String[] args) {
+
+        StreamCreatedDemo demo = new StreamCreatedDemo();
+        demo.createStream();
+
+    }
+
+
+    private void createStream() {
         //1.
-        Stream stream = Stream.of("java","c");
+        Stream stream = Stream.of("java", "c");
 
         //2.
-        String [] array = new String []{"java","python"};
+        String[] array = new String[]{"java", "python"};
         Stream stream1 = Stream.of(array);
 
+        //3.
         List<String> list = new ArrayList<>();
         Stream stream2 = list.stream();
+
+        IntStream.of(new int[]{1,4,6,9}).forEach(i->System.out.println(i));
+
+        System.out.println("-----------------------");
+        IntStream.range(3,9).forEach(System.out::println);
+        System.out.println("-----------------------");
+        IntStream.rangeClosed(3,9).forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        System.out.println(
+                IntStream.rangeClosed(1,100)
+                        .map(i->i*1)
+                        .reduce(0,Integer::sum)
+
+        );
+
 
     }
 }
