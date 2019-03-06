c9d93ab7f121843d63c38f241813fed3028e96d7
Fimics <lpjhblpj@163.com>
2019-03-06 08:35:52 +0800

    serial stream and paiallel stream

---
+++ src/main/java/com/mic/java8/stream/StreamSerialAndPaiallelDemo.java
@@ -0,0 +1,36 @@
+package com.mic.java8.stream;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 串行流与并行流
+ */
+public class StreamSerialAndPaiallelDemo {
+
+    private static int count =5000000;
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>(count);
+
+        for(int i=0;i<count;i++){
+            list.add(UUID.randomUUID().toString());
+        }
+
+        System.out.println("start sort");
+
+        long startTime = System.nanoTime();
+
+        //list.stream().sorted().count();
+        list.parallelStream().sorted().count();
+
+        long endTime =System.nanoTime();
+
+        long totalTime = endTime-startTime;
+        System.out.println("use time :" + TimeUnit.NANOSECONDS.toMillis(totalTime));
+    }
+}
