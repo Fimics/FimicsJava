d0c6bb13af856d828f5fed47a8223a9f5eda3b4e
Fimics <lpjhblpj@163.com>
2019-03-11 01:10:41 +0800

    enum singleton

---
+++ src/main/java/com/mic/thread/pattern/EnumSingleton.java
@@ -0,0 +1,27 @@
+package com.mic.thread.pattern;
+
+@SuppressWarnings("unused")
+public class EnumSingleton {
+
+    private EnumSingleton() {
+    }
+
+    private enum Holder{
+        INSTANCE;
+
+        private final EnumSingleton instance;
+
+       private Holder() {
+            instance = new EnumSingleton();
+        }
+
+        public EnumSingleton getInstance(){
+            return instance;
+        }
+    }
+
+
+    public static EnumSingleton getInstance(){
+        return  Holder.INSTANCE.getInstance();
+    }
+}
