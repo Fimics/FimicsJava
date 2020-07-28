a6f4a82d9d81b51f88d8e59786f44f5dd01028c3
lpjhblpj <lpjhblpj@163.com>
2020-07-28 22:32:56 +0800

    sort

---
+++ src/main/java/data2/stuucture/sort/cmp/SelectionSort.java
@@ -0,0 +1,34 @@
+package data2.stuucture.sort.cmp;
+
+
+import data2.stuucture.sort.Sort;
+
+public class SelectionSort<T extends Comparable<T>> extends Sort<T> {
+
+	@Override
+	protected void sort() {
+		
+//		for (int end = array.length - 1; end > 0; end--) {
+//			int max = 0;
+//			for (int begin = 1; begin <= end; begin++) {
+//				if (cmp(max, begin) <= 0) {
+//					max = begin;
+//				}
+//			}
+//			swap(max, end);
+//		}
+		
+		for (int end = array.length - 1; end > 0; end--) {
+			int max = 0;
+			for (int begin = 1; begin <= end; begin++) {
+				if (cmp(max, begin) < 0) {
+					max = begin;
+				}
+			}
+			swap(max, end);
+		}
+		
+		// 7 5 10 1 4 2 10 
+	}
+
+}
