d20fdc185102ddd9204e8d8837e90ee837272722
lpjhblpj <lpjhblpj@163.com>
2020-07-31 01:41:48 +0800

    贪心

---
+++ src/main/java/data2/stuucture/greedy/Main.java
@@ -0,0 +1,10 @@
+package data2.stuucture.greedy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/data2/stuucture/greedy/Pirate.java
@@ -0,0 +1,25 @@
+package data2.stuucture.greedy;
+
+import java.util.Arrays;
+
+/**
+ * 加勒比海盗
+ */
+public class Pirate {
+	public static void main(String[] args) {
+		int[] weights = {3, 5, 4, 10, 7, 14, 2, 11};
+		Arrays.sort(weights);
+		int capacity = 30, weight = 0, count = 0;
+		
+		for (int i = 0; i < weights.length && weight < capacity; i++) {
+			int newWeight = weight + weights[i];
+			if (newWeight <= capacity) {
+				weight = newWeight;
+				count++;
+				System.out.println(weights[i]);
+			}
+		}
+		
+		System.out.println("一共选了" + count + "件古董");
+	}
+}
