8a283852bf4a628a7886a12282b66fae13804d7f
lpjhblpj <lpjhblpj@163.com>
2020-08-02 16:54:06 +0800

    跳表

---
+++ src/main/java/data2/stuucture/skiplist/Main.java
@@ -0,0 +1,62 @@
+package data2.stuucture.skiplist;
+
+import data2.stuucture.skiplist.tools.Asserts;
+import data2.stuucture.skiplist.tools.Times;
+
+import java.util.TreeMap;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+//		SkipList<Integer, Integer> list = new SkipList<>();
+//		test(list, 30, 10);
+		
+		time();
+	}
+	
+	private static void time() {
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+//		SkipList<Integer, Integer> list = new SkipList<>();
+		int count = 100_0000;
+		int delta = 10;
+		
+//		Times.test("SkipList", () -> {
+//			test(list, count, delta);
+//		});
+		
+		Times.test("TreeMap", () -> {
+			test(map, count, delta);
+		});
+	}
+	
+	private static void test(SkipList<Integer, Integer> list, int count, int delta) {
+		for (int i = 0; i < count; i++) {
+			list.put(i, i + delta);
+		}
+//		System.out.println(list);
+		for (int i = 0; i < count; i++) {
+			Asserts.test(list.get(i) == i + delta);
+		}
+		Asserts.test(list.size() == count);
+		for (int i = 0; i < count; i++) {
+			Asserts.test(list.remove(i) == i + delta);
+		}
+		Asserts.test(list.size() == 0);
+	}
+
+	private static void test(TreeMap<Integer, Integer> map, int count, int delta) {
+		for (int i = 0; i < count; i++) {
+			map.put(i, i + delta);
+		}
+		for (int i = 0; i < count; i++) {
+			Asserts.test(map.get(i) == i + delta);
+		}
+		Asserts.test(map.size() == count);
+		for (int i = 0; i < count; i++) {
+			Asserts.test(map.remove(i) == i + delta);
+		}
+		Asserts.test(map.size() == 0);
+	}
+
+}
