3d80bf70f7dcd2a8e4789b08ffc3e5559534b619
lpjhblpj <lpjhblpj@163.com>
2021-01-09 18:50:52 +0800

    kotln map set

---
+++ src/main/kotlin/com/mic/collections/MapDemo.kt
@@ -0,0 +1,6 @@
+package com.mic.collections
+
+fun main(args:Array<String>){
+    val map = mapOf(1 to "x", 2 to "y", 3 to "z")
+    println(map)
+}
---
+++ src/main/kotlin/com/mic/collections/SetDemo.kt
@@ -0,0 +1,6 @@
+package com.mic.collections
+
+fun main(args:Array<String>){
+    val set = setOf(1,1,2,3,3)
+    println(set)
+}
