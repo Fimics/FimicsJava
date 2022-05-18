2f32538af7f702bcf499aa4594d31b5ce3155731
pengju li <lpjhblpj@163.com>
2022-05-18 20:32:38 +0800

    1

---
+++ src/main/kotlin/com/mic/p15_functional/K1_Functional.kt
@@ -0,0 +1,32 @@
+package com.mic.p15_functional
+
+sealed class Format
+
+data class Print(val text: String) : Format()
+
+object NewLine : Format()
+
+val string = listOf<Format>(Print("Hello"), NewLine, Print("Kotlin"))
+
+fun unsafeInterpreter(strs: List<Format>) {
+    strs.forEach {
+        when (it) {
+            is Print -> println(it.text)
+            is NewLine -> println("new line")
+        }
+    }
+}
+
+//纯函数方式
+fun stringInterpreter(str:List<Format>)= str.fold(""){ text,s->
+    when(s){
+        is Print -> println( text+s.text
+        is NewLine -> text+"\n"
+    }
+}
+
+
+fun main() {
+//    unsafeInterpreter(string)
+      stringInterpreter(string)
+}
